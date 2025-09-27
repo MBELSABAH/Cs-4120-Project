fd185bbe0c70cd24dd0d4baa043cde49dc8080de
Mohamed Elsabah <Mbelsabah@gmail.com>
2025-09-27 11:40:38 -0300

    Create train_baselines.py

---
+++ src/train_baselines.py
@@ -0,0 +1,7 @@
+"""
+train_baselines.py
+Classical ML models (Logistic Regression, Decision Tree, Linear, Ridge).
+Entry point: python src/train_baselines.py
+"""
+if __name__ == "__main__":
+    pass  # TODO: implement baseline training
