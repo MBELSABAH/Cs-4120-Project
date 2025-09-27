65d97e27ca9443dfe377546f872e3c7f00268464
Mohamed Elsabah <Mbelsabah@gmail.com>
2025-09-27 11:43:06 -0300

    Create train_nn.py

---
+++ src/train_nn.py
@@ -0,0 +1,7 @@
+"""
+train_nn.py
+Neural network training for classification and regression.
+Entry point: python src/train_nn.py
+"""
+if __name__ == "__main__":
+    pass  # TODO: implement NN training
